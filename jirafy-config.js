ce86a0a9f082bf8e744d91b503e416694b044e1d
jisaikorokara <jisaikorokara@Admins-MacBook-Air.local>
2023-08-13 08:24:19 +0800

    first commit

---
+++ jirafy-config.js
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="fancy-settings/source/lib/default.css">
+
+    <link rel="stylesheet" href="fancy-settings/source/css/main.css">
+    <link rel="stylesheet" href="fancy-settings/source/css/setting.css">
+    <link rel="stylesheet" href="fancy-settings/source/custom.css">
+    <script src="jquery-1.7.2.min.js"></script>
+    <script src="config.js"></script>
+  </head>
+  <body>
+    <div id="sidebar" class="fancy">
+      <img id="icon" src="icon_128.png"/>
+      <h1 id="settings-label">Jirafy</h1>
+      <div id="tab-container">
+        <div id="search-tab" class="tab">Linkify JIRA ticket numbers. Anywhere.</div>
+      </div>
+    </div>
+
+    <div id="content">
+      <div class="tab-content show">
+        <h2>Settings</h2>
+        <table class="setting group">
+          <tr>
+            <td class="setting group-name">Pages</td>
+            <td class="setting group-content">
+              <div class="setting bundle text">
+                <div class="setting container text">
+                  <label class="setting label text" for="urls_to_jirafy">URL Patterns:</label>
+                  <input class="setting element text" type="text" name="urls_to_jirafy" id="urls_to_jirafy" size="100" placeholder="git"/>
+                </div>
+              </div>
+              <div class="setting bundle description">
+                Comma-separated, can be any portion of the url.  For instance, to match any git server, use "git".  To restrict it to pull requests, use "git.example.com/myorg/myproject/pull".  Regexes NOT currently supported.
+              </div>
+              <div class="setting bundle text">
+                <div class="setting container text">
+                  <label class="setting label text" for="ignore_elements">Ignore Elements</label>
+                  <input type="text" name="ignore_elements" id="ignore_elements" size="100" placeholder="a,textarea,code"/>
+                </div>
+              </div>
+              <div class="setting bundle description">
+                Comma-separated elements that you would like to ignore. If you know you won't need to Jirafy any links in a particular element this could speed up the process. Example: "a,textarea,code,div"
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="setting group-name">JIRA </td>
+            <td class="setting group-content">
+              <div class="setting bundle text">
+                <div class="setting container text">
+                  <label class="setting label text" for="project_keys">Project Keys:</label>
+                  <input type="text" name="project_keys" id="project_keys" size="100" placeholder="ANDROID,IOS"/>
+                  <div id="project_checkboxes_container"></div>
+                </div>
+              </div>
+              <div class="setting bundle description">
+                Comma-separated project keys that you care about.  You can use the "Detect Projects" link below to query your JIRA instance and load its projects.  The resulting checkboxes will then populate this field.
+              </div>
+              <div class="setting container text">
+                <label class="setting label text" for="jira_server">JIRA Server:</label>
+                <input type="text" name="jira_server" id="jira_server" placeholder="https://jira.example.com/"/>
+                <a id="loadProjectKeys">Detect Projects</a>
+              </div>
+              <div class="setting bundle description">
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="setting group-name">Links</td>
+            <td class="setting group-content">
+              <div class="setting bundle text">
+                <div class="setting container text">
+                  <label class="setting label text" for="new_window">Open Links in a new window</label>
+                  <input type="checkbox" name="new_window" id="new_window" />
+                </div>
+              </div>
+              <div class="setting bundle description"></div>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </body>
+</html>
